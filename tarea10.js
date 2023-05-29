4d2a94ebff0f8e38f183b4c9ab6a6ca17bdae9c5
PankyFoxdieLeo <130614870+PankyFoxdieLeo@users.noreply.github.com>
2023-05-29 19:03:37 -0300

    Add files via upload

---
+++ tarea10.js
@@ -0,0 +1,15 @@
+const helado=230; // precio
+
+let topping="kitkat";
+let precio;
+let preciofinal;
+
+if (topping == "kitkat"){
+    precio=10
+}   else if (topping == "oreo"){
+    precio=15
+}   else if (topping == "kinder"){
+    precio=25
+}
+
+    preciofinal=helado + precio
